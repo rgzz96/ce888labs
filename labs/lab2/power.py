c7de983d2398f6b8bd72db54cc7cf261eedba7c5
rgzz96 <r.gzzmtz96@gmail.com>
2019-02-11 11:08:31 +0000

    Labs

--- labs/lab2/power.py
+++ labs/lab2/power.py
@@ -4,22 +4,36 @@ Created on Mon Jan 21 14:36:54 2019
 
 @author: rg18217
 """
+def p_value(sample1, sample2, reps, size):
+	count = 0
+
+	sample = np.concatenate((sample1, sample2))
+
+	for i in range(reps):
+		samp = np.random.choice(sample, sample.size * 2, replace = False)
+		samp1 = samp[:sample.size]
+		samp2 = samp[smaple.size:]
+
+		t_perm = samp2.mean() -samp1.mean()
+
+		if t_perm > size:
+			count += 1
+
+	return count/reps
+
+
 
 def power(sample1, sample2, reps, size, alpha):
     count = 0
-    observed = sample2.mean() - sample1.mean()
     
     for i in range(reps):
-        s1 = np.random.choice(sample1, size = size, Replace = True)
-        s2 = np.random.choice(sample1, size = size, Replace = True)
+        samp1 = np.random.choice(sample1, sample1.size, Replace = True)
+        samp2 = np.random.choice(sample2, sample2.size, Replace = True)
+    	pvalue = p_value(samp1, samp2, reps, size)
     
-    perm = s1.mean() - s2.mean()
-    
-    if perm < 1-alpha:
-        count+=1
-    
-    ratio = count/reps
-    
-    return ratio
+    if pvalue < (1-alpha):
+    	count +=1
+     
+    return count/teps
 
     
